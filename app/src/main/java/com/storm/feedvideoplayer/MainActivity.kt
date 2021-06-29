1f63f9ae95390c812f0e71a31b1a80335be7d474
Lazy-Lad <code.with.lazylad@gmail.com>
2021-06-30 02:56:27 +0530

    progress half

--- app/src/main/java/com/storm/feedvideoplayer/MainActivity.kt
+++ app/src/main/java/com/storm/feedvideoplayer/MainActivity.kt
@@ -2,11 +2,32 @@ package com.storm.feedvideoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.storm.feedvideoplayer.databinding.ActivityMainBinding
+import com.storm.feedvideoplayer.util.jsonFileToList
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var view: View
+    private lateinit var adapter:VideoPlayerRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // code to be written.
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        view = binding.root
+        setContentView(view)
+        val videoList = jsonFileToList("videoDetails.json")
+        Log.d("JSON_FILE", videoList?.get(0)!!.toString())
+        if(videoList.isNotEmpty()){
+            adapter = VideoPlayerRecyclerViewAdapter(videoList)
+            binding.recyclerView.layoutManager = LinearLayoutManager(this)
+            binding.recyclerView.adapter = adapter
+        }else{
+            Toast.makeText(this, "We don't have data right now.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/storm/feedvideoplayer/data/VideoDetailModel.kt
@@ -0,0 +1,9 @@
+package com.storm.feedvideoplayer.data
+
+data class VideoDetailModel(
+    val description: String,
+    val sources:String,
+    val subtitle: String,
+    val thumb: String,
+    val title: String
+)
